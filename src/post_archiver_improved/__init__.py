4e383fadb56b6091bede065cd109d0390cb0d4bc
sadadYes <syaddadpunya@gmail.com>
2025-08-09 18:56:07 +0700

    chore: bump version release v0.3.0

--- src/post_archiver_improved/__init__.py
+++ src/post_archiver_improved/__init__.py
@@ -5,7 +5,7 @@ A lightweight, zero-dependency Python tool for archiving YouTube community posts
 with support for comments extraction and image downloading.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __author__ = "sadadYes"
 __description__ = (
     "A Python tool for archiving YouTube community posts with zero dependencies"
